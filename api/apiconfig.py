01589a7275f2984a5b87b9254005150112949ecf
Daniel Machado <danielmcis@gmail.com>
2018-12-03 23:35:46 -0500

    Se cambia nivel de Debug para Flask.

--- api/apiconfig.py
+++ api/apiconfig.py
@@ -20,6 +20,23 @@ import functools
 
 from werkzeug.wrappers import ETagResponseMixin, Response
 
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'DEBUG',
+        'handlers': ['wsgi']
+    }
+})
+
 app = Flask(__name__)
 auth = HTTPBasicAuth()
 
